f1ac256cec364385b05df987ca255d212380d1e3
Kyrylo Shmidt <kshmidt@digma.ai>
2023-03-31 11:17:14 +0200

    Update Installation Wizard styles
    Add environment variable to skip installation step

--- src/components/InstallationWizard/CodeSnippet/styles.ts
+++ src/components/InstallationWizard/CodeSnippet/styles.ts
@@ -18,7 +18,7 @@ export const Container = styled.div<ContainerProps>`
 export const Code = styled.code`
   ${({ theme }) => getCodeFont(theme.codeFont)}
   font-weight: 500;
-  font-size: 9px;
+  font-size: 12px;
   line-height: 20px;
   letter-spacing: -0.1px;
   color: #dadada;
