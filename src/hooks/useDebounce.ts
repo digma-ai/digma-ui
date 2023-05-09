ed71209cb447aff5ff6acd5249232f3df38fe810
Kyrylo Shmidt <kshmidt@digma.ai>
2023-05-09 17:43:17 +0200

    Hide spinner on email editing finish without changes

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useDebounce = <T>(value: T, delay: number) => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
     const handler = setTimeout(() => {
