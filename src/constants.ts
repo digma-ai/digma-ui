c9315a095c27298545a545a8bd7dd3067eb39f8a
Kyrylo Shmidt <kshmidt@digma.ai>
2023-06-28 21:54:52 +0200

    Fix Getting Started video link

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const SLACK_WORKSPACE_URL =
   "https://join.slack.com/t/continuous-feedback/shared_invite/zt-1hk5rbjow-yXOIxyyYOLSXpCZ4RXstgA";
 
 export const GETTING_STARTED_VIDEO_URL =
-  "https://www.youtube.com/watch?v=jzBEXfCrnlg";
+  "https://www.youtube.com/watch?v=F1Y0kETn-QQ";
