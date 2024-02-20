b134938c36ce1194d019d470adc8786f947fb034
opoliarush <156646693+opoliarush@users.noreply.github.com>
2024-02-20 12:10:08 +0200

    Update src/components/common/v3/Card/styles.ts
    Co-authored-by: Kyrylo Shmidt <119138536+kshmidt-digma@users.noreply.github.com>

--- src/components/common/v3/Card/styles.ts
+++ src/components/common/v3/Card/styles.ts
@@ -3,34 +3,13 @@ import { DefaultTheme } from "styled-components/dist/types";
 import { darkTheme } from "../../App/themes/darkTheme";
 import { lightTheme } from "../../App/themes/lightTheme";
 
-const getStroke = (theme: DefaultTheme) => {
-  switch (theme.mode) {
-    case "light":
-      return lightTheme.v3.stroke.primary;
-    case "dark":
-    case "dark-jetbrains":
-      return darkTheme.v3.stroke.primary;
-  }
-};
-
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 4px;
-  border: 1px solid ${({ theme }) => getStroke(theme)};
-  background: ${({ theme }) => {
-    switch (theme.mode) {
-      case "light":
-        return lightTheme.v3.surface.secondary;
-      case "dark":
-      case "dark-jetbrains":
-        return darkTheme.v3.surface.secondary;
-    }
-  }};
-
-  &:last-child {
-    border: none;
-  }
+  border: 1px solid ${({ theme }) => theme.colors.v3.stroke.primary};
+  background: ${({ theme }) => theme.colors.v3.surface.secondary};
+  font-size: 14px;
 `;
 
 export const Content = styled.div`
