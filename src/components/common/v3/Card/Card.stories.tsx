473ddc27fb8c092375f968796fc65c1d8c8c3305
Kyrylo Shmidt <kshmidt@digma.ai>
2024-02-20 11:23:35 +0100

    Improve card

--- src/components/common/v3/Card/Card.stories.tsx
+++ src/components/common/v3/Card/Card.stories.tsx
@@ -3,7 +3,7 @@ import { Card } from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Card> = {
-  title: "v3/Common/Card",
+  title: "Common/v3/Card",
   component: Card,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
--- src/components/common/v3/Card/styles.ts
+++ src/components/common/v3/Card/styles.ts
@@ -1,27 +1,27 @@
 import styled from "styled-components";
-import { DefaultTheme } from "styled-components/dist/types";
-import { darkTheme } from "../../App/themes/darkTheme";
-import { lightTheme } from "../../App/themes/lightTheme";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 4px;
   border: 1px solid ${({ theme }) => theme.colors.v3.stroke.primary};
-  background: ${({ theme }) => theme.colors.v3.surface.secondary};
-  font-size: 14px;
+  background: ${({ theme }) => theme.colors.v3.surface.primary};
 `;
 
-export const Content = styled.div`
+const Section = styled.div`
   padding: 8px;
 `;
 
-export const Header = styled.div`
+export const Content = styled(Section)`
+  padding: 8px;
+`;
+
+export const Header = styled(Section)`
   border-bottom: 1px solid ${({ theme }) => theme.colors.v3.stroke.primary};
   padding: 8px;
 `;
 
-export const Footer = styled.div`
+export const Footer = styled(Section)`
   border-top: 1px solid ${({ theme }) => theme.colors.v3.stroke.primary};
   padding: 8px;
 `;
