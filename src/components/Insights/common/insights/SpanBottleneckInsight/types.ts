836a9d917dd7297089e178c9cd9570a277334305
Kyrylo Shmidt <kshmidt@digma.ai>
2024-02-27 16:29:41 +0100

    Remove obsolete file

--- src/components/Insights/common/insights/SpanBottleneckInsight/types.ts
+++
@@ -1,6 +0,0 @@
-import { EndpointSlowestSpansInsight, InsightProps } from "../../../types";
-
-export interface SpanBottleneckInsightProps extends InsightProps {
-  insight: EndpointSlowestSpansInsight;
-  onAssetLinkClick: (spanCodeObjectId: string) => void;
-}
--- src/components/Insights/common/insights/SpanNexusInsight/SpanNexusInsight.stories.tsx
+++ src/components/Insights/common/insights/SpanNexusInsight/SpanNexusInsight.stories.tsx
@@ -4,7 +4,7 @@ import { mockedSpanNexusInsight } from "./mockData";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof SpanNexusInsight> = {
-  title: "Insights/Common/Insights/SpanNexusInsight",
+  title: "Insights/common/insights/SpanNexusInsight",
   component: SpanNexusInsight,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
