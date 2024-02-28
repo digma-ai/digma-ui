0b8d91f06ca9450d3b0921e62cf05bb822212760
olehp <opoliarush@digma.ai>
2024-02-28 13:55:35 +0200

    Traffic Insight

---
+++ src/components/Insights/common/insights/TrafficInsight/index.tsx
@@ -0,0 +1,67 @@
+import { roundTo } from "../../../../../utils/roundTo";
+import { Tag } from "../../../../common/v3/Tag";
+import { TrafficInsightProps } from "../../../TrafficInsight/types";
+import { InsightType } from "../../../types";
+import { InsightCard } from "../../InsightCard";
+import {
+  KeyValue,
+  KeyValueContainer
+} from "../../InsightCard/KeyValueContainer";
+import * as s from "./styles";
+
+const suffixes = [
+  { value: 1, symbol: "" },
+  { value: 1e3, symbol: "k" },
+  { value: 1e6, symbol: "M" },
+  { value: 1e9, symbol: "G" },
+  { value: 1e12, symbol: "T" },
+  { value: 1e15, symbol: "P" },
+  { value: 1e18, symbol: "E" }
+];
+
+const getValueString = (requestCount: number): string => {
+  const suffix = [...suffixes]
+    .reverse()
+    .find((suffix) => requestCount >= suffix.value);
+
+  if (suffix) {
+    return `~${roundTo(requestCount / suffix.value, 2)}${suffix.symbol}`;
+  }
+
+  return `~${roundTo(requestCount, 2)}`;
+};
+
+const getDescription = (insightType: InsightType): string => {
+  switch (insightType) {
+    case InsightType.LowUsage:
+      return "Servicing a low number of requests";
+    case InsightType.NormalUsage:
+      return "Servicing an average number of requests";
+    case InsightType.HighUsage:
+      return "Servicing a high number of requests";
+    default:
+      return "";
+  }
+};
+
+export const TrafficInsight = (props: TrafficInsightProps) => {
+  const valueString = getValueString(props.insight.maxCallsIn1Min);
+
+  return (
+    <InsightCard
+      insight={props.insight}
+      content={
+        <s.ContentContainer>
+          <s.Description>{getDescription(props.insight.type)}</s.Description>
+          <KeyValueContainer>
+            <KeyValue label={"Duration"}>
+              <Tag content={`${valueString}/min`} />
+            </KeyValue>
+          </KeyValueContainer>
+        </s.ContentContainer>
+      }
+      onRecalculate={props.onRecalculate}
+      onRefresh={props.onRefresh}
+    />
+  );
+};
