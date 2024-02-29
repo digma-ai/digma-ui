6e846903613cb9b598dc47c1d59c5550e2690214
olehp <opoliarush@digma.ai>
2024-02-29 15:11:48 +0200

    Endpoint Side

---
+++ src/components/Insights/common/insights/SpanNPlusOneInsight/index.tsx
@@ -0,0 +1,138 @@
+import { useContext, useState } from "react";
+import { getDurationString } from "../../../../../utils/getDurationString";
+import { sendTrackingEvent } from "../../../../../utils/sendTrackingEvent";
+import { trimEndpointScheme } from "../../../../../utils/trimEndpointScheme";
+import { ConfigContext } from "../../../../common/App/ConfigContext";
+import { InfoCircleIcon } from "../../../../common/icons/InfoCircleIcon";
+import { Tooltip } from "../../../../common/v3/Tooltip";
+import { trackingEvents } from "../../../tracking";
+import { InsightType, Trace } from "../../../types";
+import { InsightCard } from "../../InsightCard";
+import { ColumnsContainer } from "../../InsightCard/ColumnsContainer";
+import { KeyValue } from "../../InsightCard/KeyValue";
+import { ListItem } from "../../InsightCard/ListItem";
+import { Select } from "../../InsightCard/Select";
+import * as s from "./styles";
+import { SpanNPlusOneInsightProps } from "./types";
+
+export const SpanNPlusOneInsight = (props: SpanNPlusOneInsightProps) => {
+  const {
+    insight: { type, endpoints, ticketLink }
+  } = props;
+
+  const config = useContext(ConfigContext);
+  const [selectedEndpoint, setSelectedEndpoint] = useState(
+    props.insight.endpoints.length ? props.insight.endpoints[0] : null
+  );
+
+  const handleSpanLinkClick = (spanCodeObjectId?: string) => {
+    spanCodeObjectId && props.onAssetLinkClick(spanCodeObjectId, type);
+  };
+
+  const handleTraceButtonClick = (
+    trace: Trace,
+    insightType: InsightType,
+    spanCodeObjectId?: string
+  ) => {
+    props.onTraceButtonClick(trace, insightType, spanCodeObjectId);
+  };
+
+  const handleCreateJiraTicketButtonClick = (event: string) => {
+    sendTrackingEvent(trackingEvents.JIRA_TICKET_INFO_BUTTON_CLICKED, {
+      insightType: type
+    });
+    props.onJiraTicketCreate &&
+      props.onJiraTicketCreate(props.insight, undefined, event);
+  };
+
+  const spanName = props.insight.clientSpanName || undefined;
+  const spanCodeObjectId = props.insight.clientSpanCodeObjectId || undefined;
+  const traceId = props.insight.traceId;
+
+  return (
+    <InsightCard
+      insight={props.insight}
+      onGoToTrace={
+        config.isJaegerEnabled && traceId
+          ? () =>
+              handleTraceButtonClick(
+                {
+                  name: spanName,
+                  id: traceId
+                },
+                props.insight.type,
+                spanCodeObjectId
+              )
+          : undefined
+      }
+      content={
+        <s.ContentContainer>
+          <s.Description>
+            Effected Endpoints ({endpoints.length})
+            <Select
+              value={selectedEndpoint?.endpointInfo.entrySpanCodeObjectId}
+              onChange={(selectedOption) => {
+                const selected =
+                  endpoints.find(
+                    (x) =>
+                      x.endpointInfo.entrySpanCodeObjectId === selectedOption
+                  ) || null;
+
+                setSelectedEndpoint(selected);
+              }}
+              options={props.insight.endpoints.map((x) => {
+                const spanCodeObjectId = x.endpointInfo.entrySpanCodeObjectId;
+                const route = trimEndpointScheme(x.endpointInfo.route);
+                return {
+                  label: route,
+                  customContent: (
+                    <s.SelectedItem>
+                      {x.endpointInfo.serviceName}
+                      <ListItem
+                        name={route}
+                        onClick={() => handleSpanLinkClick(spanCodeObjectId)}
+                      />
+                    </s.SelectedItem>
+                  ),
+                  value: spanCodeObjectId
+                };
+              })}
+            />
+          </s.Description>
+          {selectedEndpoint && (
+            <ColumnsContainer>
+              <KeyValue label={"Repeats"}>
+                {selectedEndpoint.occurrences}
+              </KeyValue>
+              <KeyValue
+                label={
+                  <Tooltip
+                    title={"The amount of requests affected by this issue."}
+                  >
+                    <s.InfoContainer>
+                      <div>Requests</div>
+                      <InfoCircleIcon color={"currentColor"} size={12} />
+                    </s.InfoContainer>
+                  </Tooltip>
+                }
+              >
+                {selectedEndpoint.requestPercentage}%
+              </KeyValue>
+              <KeyValue label={"Duration"}>
+                {getDurationString(selectedEndpoint.duration)}
+              </KeyValue>
+            </ColumnsContainer>
+          )}
+        </s.ContentContainer>
+      }
+      onRecalculate={props.onRecalculate}
+      onRefresh={props.onRefresh}
+      onJiraButtonClick={handleCreateJiraTicketButtonClick}
+      jiraTicketInfo={{
+        ticketLink,
+        isHintEnabled: props.isJiraHintEnabled,
+        spanCodeObjectId: props.insight.spanInfo?.spanCodeObjectId
+      }}
+    />
+  );
+};
---
+++ src/components/Insights/common/insights/SpanNPlusOneInsight/styles.ts
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import {
+  caption1RegularTypography,
+  footnoteRegularTypography
+} from "../../../../common/App/typographies";
+
+export const Stats = styled.span`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px 24px;
+`;
+
+export const Stat = styled.span`
+  display: flex;
+  gap: 4px;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  color: ${({ theme }) => {
+    switch (theme.mode) {
+      case "light":
+        return "#49494d";
+      case "dark":
+      case "dark-jetbrains":
+        return "#dadada";
+    }
+  }};
+`;
+
+export const SpanContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const EndpointList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const Endpoint = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const Name = styled.span`
+  font-weight: 500;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  max-width: fit-content;
+`;
+
+export const CriticalityValue = styled.span`
+  display: flex;
+  gap: 4px;
+`;
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  color: ${({ theme }) => theme.colors.v3.text.secondary};
+
+  ${caption1RegularTypography}
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`;
+
+export const SelectedItem = styled.div`
+  display: flex;
+  align-items: center;
+  ${footnoteRegularTypography}
+`;
