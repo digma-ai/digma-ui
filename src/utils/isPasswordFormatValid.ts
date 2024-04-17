6f3a350c85fa286baa6f65ee2044e184fe66bf8d
olehp <opoliarush@digma.ai>
2024-04-17 13:29:22 +0300

    Fixed validation

--- src/utils/isPasswordFormatValid.ts
+++ src/utils/isPasswordFormatValid.ts
@@ -1,5 +1,5 @@
 const PASSWORD_REGEX =
-  /^(?=.*[A-Za-z0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,}$/gm;
+  /^(?=.*[A-Za-z0-9])(?=.*[!@#$%^&*-_])[A-Za-z0-9!@#$%^&*-_]{6,}$/gm;
 
 export const isValidPasswordFormat = (password: string): boolean => {
   return new RegExp(PASSWORD_REGEX).test(password);
