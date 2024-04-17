45a812f06476bc501a5d6ab8537eb5dc40a3a667
olehp <opoliarush@digma.ai>
2024-04-17 13:45:34 +0300

    add comment

--- src/utils/isPasswordFormatValid.ts
+++ src/utils/isPasswordFormatValid.ts
@@ -1,3 +1,4 @@
+// password should have at least 6 symbols and could be alphanumeric with at least one spec symbol
 const PASSWORD_REGEX =
   /^(?=.*[A-Za-z0-9])(?=.*[!@#$%^&*-])[A-Za-z0-9!@#$%^&*-]{6,}$/gm;
 
