739954fa7ae79d7ddb97df15a633d35c3510a7b9
Kyrylo Shmidt <119138536+kshmidt-digma@users.noreply.github.com>
2024-07-12 10:19:59 +0200

    Update Error screen styles (#933)
    * Update Error screen styles

--- src/components/common/ErrorScreen/styles.ts
+++ src/components/common/ErrorScreen/styles.ts
@@ -1,11 +1,25 @@
 import styled from "styled-components";
+import { bodyRegularTypography } from "../App/typographies";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 8px;
+  gap: 12px;
   height: 100%;
+`;
+
+export const TextContainer = styled.div`
+  ${bodyRegularTypography}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
   text-align: center;
+  color: ${({ theme }) => theme.colors.v3.text.secondary};
+`;
+
+export const Title = styled.span`
+  color: ${({ theme }) => theme.colors.v3.text.primary};
 `;
