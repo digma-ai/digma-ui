be37710e95301425c40cc48d36f12ccd2fbaa265
olehp <opoliarush@digma.ai>
2024-07-31 11:01:27 +0300

    comment download

--- src/components/Dashboard/Reports/ReportsHeader/Ribbon/index.tsx
+++ src/components/Dashboard/Reports/ReportsHeader/Ribbon/index.tsx
@@ -1,16 +1,15 @@
 import { sendUserActionTrackingEvent } from "../../../../../utils/actions/sendUserActionTrackingEvent";
-import { DownloadIcon } from "../../../../common/icons/12px/DownloadIcon";
 import { RecalculateIcon } from "../../../../common/icons/16px/RecalculateIcon";
 import { NewButton } from "../../../../common/v3/NewButton";
 import { trackingEvents } from "../../tracking";
 import * as s from "./styles";
 import { RibbonProps } from "./types";
 
-export const Ribbon = ({ onDownload, onRefresh }: RibbonProps) => {
-  const handleDownloadClick = () => {
-    sendUserActionTrackingEvent(trackingEvents.DOWNLOAD_REPORT_CLICKED);
-    onDownload();
-  };
+export const Ribbon = ({ onRefresh }: RibbonProps) => {
+  // const handleDownloadClick = () => {
+  //   sendUserActionTrackingEvent(trackingEvents.DOWNLOAD_REPORT_CLICKED);
+  //   onDownload();
+  // };
 
   const handleRefreshClick = () => {
     sendUserActionTrackingEvent(trackingEvents.REFRESH_DATA_CLICKED);
@@ -28,13 +27,13 @@ export const Ribbon = ({ onDownload, onRefresh }: RibbonProps) => {
           label="Refresh data"
           onClick={handleRefreshClick}
         />
-        <s.DownloadButton
+        {/* <s.DownloadButton
           buttonType="tertiary"
           icon={DownloadIcon}
           label="Download PDF"
           iconPosition="right"
           onClick={handleDownloadClick}
-        />
+        /> */}
       </s.ButtonContainer>
     </s.Container>
   );
