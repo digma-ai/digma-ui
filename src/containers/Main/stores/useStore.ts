64390f75c8269b1011d0844a2972e3b02a915a11
olehp <opoliarush@digma.ai>
2024-08-30 14:40:20 +0300

    added mutable actions

--- src/containers/Main/stores/useStore.ts
+++ src/containers/Main/stores/useStore.ts
@@ -1,22 +1,25 @@
 import { create } from "zustand";
-import { withActions, withSlices } from "zustand-slices";
+import { withSlices } from "zustand-slices";
 import { Scope } from "../../../components/common/App/types";
 import { globalSlice } from "../slices/globalSlice";
 import { insightsSlice } from "../slices/insightSlice";
 import { createSelectors } from "./createSelectors";
+import { withMutableActions } from "./withMutableActions";
 
 export const useStore = createSelectors(
   create(
-    withActions(withSlices(globalSlice, insightsSlice), {
-      setScope: (scope: Scope) => (state) => {
-        return state.global.scope?.span?.spanCodeObjectId !==
+    withMutableActions(withSlices(globalSlice, insightsSlice), {
+      setScope: (scope: Scope) => (_, set) => {
+        set((state) =>
+          state.global.scope?.span?.spanCodeObjectId !==
           scope.span?.spanCodeObjectId
-          ? {
-              ...state,
-              global: { ...state.global, scope },
-              insights: { ...state.insights, page: 0, search: "" }
-            }
-          : state;
+            ? {
+                ...state,
+                global: { ...state.global, scope },
+                insights: { ...state.insights, page: 0, search: "" }
+              }
+            : state
+        );
       }
     })
   )
