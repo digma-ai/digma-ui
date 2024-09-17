4ba91c2759303052a81a78282cb413aea32bf50c
Kyrylo Shmidt <kshmidt@digma.ai>
2024-09-17 21:31:16 +0200

    Fix feature flag version

--- src/featureFlags.ts
+++ src/featureFlags.ts
@@ -15,7 +15,7 @@ export const featureFlagMinBackendVersions: Record<FeatureFlag, string> = {
   [FeatureFlag.ARE_ISSUES_SERVICES_FILTERS_ENABLED]: "0.3.103",
   [FeatureFlag.ARE_EXTENDED_ASSETS_FILTERS_ENABLED]: "0.3.107",
   [FeatureFlag.IS_NEW_IMPACT_SCORE_CALCULATION_ENABLED]: "0.3.107",
-  [FeatureFlag.IS_METRICS_REPORT_ENABLED]: "0.3.120"
+  [FeatureFlag.IS_METRICS_REPORT_ENABLED]: "0.3.120-alpha.15"
 };
 
 export const getFeatureFlagValue = (
