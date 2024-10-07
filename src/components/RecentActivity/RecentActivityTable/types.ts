e9d98856489670c60eb9d2d6a8579caff7db765d
opoliarush <156646693+opoliarush@users.noreply.github.com>
2024-10-07 11:37:55 +0300

    Update src/components/RecentActivity/RecentActivityTable/types.ts
    Co-authored-by: Kyrylo Shmidt <119138536+kshmidt-digma@users.noreply.github.com>

--- src/components/RecentActivity/RecentActivityTable/types.ts
+++ src/components/RecentActivity/RecentActivityTable/types.ts
@@ -13,7 +13,7 @@ export interface RecentActivityTableProps {
 export interface ColumnMeta {
   width: string | number;
   minWidth?: string | number;
-  textAlign?: "center";
+  textAlign?: "left" | "center" | "right";
 }
 
 export interface TableHeadProps {
