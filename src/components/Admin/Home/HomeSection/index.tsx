fdeabb43f2ef242a551c51ef86c5c9c9a6986e21
Kyrylo Shmidt <kshmidt@digma.ai>
2025-02-03 15:41:30 +0100

    Update styles

--- src/components/Admin/Home/HomeSection/index.tsx
+++ src/components/Admin/Home/HomeSection/index.tsx
@@ -1,8 +1,12 @@
 import * as s from "./styles";
 import type { HomeSectionProps } from "./types";
 
-export const HomeSection = ({ title, children }: HomeSectionProps) => (
-  <s.Section>
+export const HomeSection = ({
+  title,
+  children,
+  className
+}: HomeSectionProps) => (
+  <s.Section className={className}>
     <s.Header>{title}</s.Header>
     {children}
   </s.Section>
--- src/components/Admin/Home/HomeSection/types.ts
+++ src/components/Admin/Home/HomeSection/types.ts
@@ -3,4 +3,5 @@ import type { ReactNode } from "react";
 export interface HomeSectionProps {
   title: string;
   children: ReactNode;
+  className?: string;
 }
