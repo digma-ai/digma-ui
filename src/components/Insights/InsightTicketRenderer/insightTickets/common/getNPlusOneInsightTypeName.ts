616b51835ae38db9d8b68d491b49cd4b57ba5ad6
Kyrylo Shmidt <kshmidt@digma.ai>
2025-02-18 10:38:45 +0100

    Update insight ticket description

--- src/components/Insights/InsightTicketRenderer/insightTickets/common/getNPlusOneInsightTypeName.ts
+++ src/components/Insights/InsightTicketRenderer/insightTickets/common/getNPlusOneInsightTypeName.ts
@@ -1,7 +1,7 @@
 export const getNPlusOneInsightTypeName = (subType?: string) => {
   switch (subType) {
     case "repeatedQueries":
-      return "Suspected N+1 Query";
+      return "Suspected N+1";
     case "repeatedInserts":
       return "Repeated inserts";
     default:
