2363688aecf006ff4f5ebb3f170805f72c1a7df3
Kyrylo Shmidt <kshmidt@digma.ai>
2025-03-04 20:50:30 +0100

    Hide Highlights widgets

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -1,30 +1,30 @@
-import { getFeatureFlagValue } from "../../featureFlags";
-import { useGetAboutQuery } from "../../redux/services/digma";
-import { FeatureFlag } from "../../types";
-import { Impact } from "./Impact";
+// import { getFeatureFlagValue } from "../../featureFlags";
+// import { useGetAboutQuery } from "../../redux/services/digma";
+// import { FeatureFlag } from "../../types";
+// import { Impact } from "./Impact";
 import { Performance } from "./Performance";
-import { Scaling } from "./Scaling";
-import { TopIssues } from "./TopIssues";
+// import { Scaling } from "./Scaling";
+// import { TopIssues } from "./TopIssues";
 import * as s from "./styles";
 
 export const Highlights = () => {
-  const { data: about } = useGetAboutQuery();
+  // const { data: about } = useGetAboutQuery();
 
-  const areImpactHighlightsVisible = Boolean(
-    about &&
-      getFeatureFlagValue(about, FeatureFlag.IS_HIGHLIGHTS_IMPACT_ENABLED)
-  );
-  const areScalingHighlightsVisible = Boolean(
-    about &&
-      getFeatureFlagValue(about, FeatureFlag.IS_HIGHLIGHTS_SCALING_ENABLED)
-  );
+  // const areImpactHighlightsVisible = Boolean(
+  //   about &&
+  //     getFeatureFlagValue(about, FeatureFlag.IS_HIGHLIGHTS_IMPACT_ENABLED)
+  // );
+  // const areScalingHighlightsVisible = Boolean(
+  //   about &&
+  //     getFeatureFlagValue(about, FeatureFlag.IS_HIGHLIGHTS_SCALING_ENABLED)
+  // );
 
   return (
     <s.Container>
-      <TopIssues />
+      {/* <TopIssues /> */}
       <Performance />
-      {areImpactHighlightsVisible && <Impact />}
-      {areScalingHighlightsVisible && <Scaling />}
+      {/* {areImpactHighlightsVisible && <Impact />} */}
+      {/* {areScalingHighlightsVisible && <Scaling />} */}
     </s.Container>
   );
 };
