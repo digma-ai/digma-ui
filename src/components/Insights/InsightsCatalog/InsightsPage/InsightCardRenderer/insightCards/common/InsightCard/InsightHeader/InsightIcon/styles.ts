9ada3769d49606df910888ae95a9af117fae0f10
Kyrylo Shmidt <kshmidt@digma.ai>
2025-05-02 16:57:35 +0200

    Remove obsolete styles

--- src/components/Insights/InsightsCatalog/InsightsPage/InsightCardRenderer/insightCards/common/InsightCard/InsightHeader/InsightIcon/styles.ts
+++ src/components/Insights/InsightsCatalog/InsightsPage/InsightCardRenderer/insightCards/common/InsightCard/InsightHeader/InsightIcon/styles.ts
@@ -3,17 +3,3 @@ import styled from "styled-components";
 export const InsightIconContainer = styled.div`
   display: flex;
 `;
-
-export const TitleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  width: 180px;
-  padding: 8px;
-`;
-
-export const Divider = styled.div`
-  height: 1px;
-  background: ${({ theme }) => theme.colors.v3.stroke.tertiary};
-  width: 100%;
-`;
